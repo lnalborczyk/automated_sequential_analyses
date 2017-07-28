52026bfa8171fb2f1e09a8119576c63418b5045b
lnalborczyk <ladislas.nalborczyk@gmail.com>
2017-07-28 13:48:39 +0200

    add boundaries for H0

--- scripts/BFF_predictions.R
+++ scripts/BFF_predictions.R
@@ -1,18 +1,15 @@
 if(!require(BayesFactor)){install.packages("BayesFactor")}
-if(!require(magrittr)){install.packages("tidyverse")}
+if(!require(tidyverse)){install.packages("tidyverse")}
 library(BayesFactor)
 library(tidyverse)
 
 rm(list = ls() ) # cleaning working environment
 
-seqBF <- function(cohensd = 0.5, prior = 0.5, nSims = 10, boundary = Inf, nmin = 10, nmax = 100){
+seqBF <- function(cohensd = 0.5, prior = 0.5, nSims = 100, boundary = Inf, nmin = 10, nmax = 100){
     
     options(scipen = 999) # disable scientific notation for numbers
     
-    if(nmin<10) nmin <- 10 # force nmin to be at leat equal to 10
-    if(prior==0) prior <- 0.01 # avoid to multiply the BF by zero...
-    
-    prior <- 2 * prior # in order to scale priors on 1
+    if (nmin < 10) nmin <- 10 # force nmin to be at leat equal to 10
     
     ns <- seq(nmin, nmax, by = 1)
     
@@ -21,7 +18,7 @@ seqBF <- function(cohensd = 0.5, prior = 0.5, nSims = 10, boundary = Inf, nmin =
     
     res.counter <- 1
     
-    for(i in 1:nSims){
+    for (i in 1:nSims) {
         
         x <- rnorm(n = nmax, mean = 0, sd = 1)
         y <- rnorm(n = nmax, mean = 0 + cohensd, sd = 1)
@@ -31,20 +28,27 @@ seqBF <- function(cohensd = 0.5, prior = 0.5, nSims = 10, boundary = Inf, nmin =
         # res0 keeps the accumulating sample variables from this specific run
         res0 <- matrix(NA, nrow = length(ns), ncol = ncol(res), dimnames = dimnames(res) )
         
-        for (n in ns) {
+        for (n in ns){
             
+            # subestting data
             samp <- maxsamp[1:n,]
+
+            # computing the BF
+            logBF <- BayesFactor::ttestBF(samp[,1], samp[,2]) %>% as.vector %>% as.numeric
             
-            t <- t.test(samp[,1], samp[,2])$statistic # performing the t-test
+            # including a penalisation to illustrates the expected consequences of the SEAB
+            # NB: this simple penalisation does not aim to be realistic,
+            # it is for illustrative purposes only...
             
-            logBF <- BayesFactor::ttest.tstat(t, nrow(samp), nrow(samp),
-                nullInterval = NULL, simple = TRUE)
-            
-            if(n > nmin){ # include a penalization of b_n by the value of b_n-1
+            if ( n > nmin && !is.na(prior ) ) { # starting from nmin
                 
-                logBF_0 <- res0[n-nmin, 5] # define BF of n-1
-                logBF = mean(logBF, logBF_0) * prior # simply taking the mean of BF n-1 and current BF
+                # extracting previous BF
+                logBF_0 <- res0[n-nmin, 5]
                 
+                # simply computing the mean of the last and current BFs
+                # and "strengthening" it by the "prior" expectancies of the experimenter
+                logBF = mean(logBF, logBF_0) * (2 * prior)
+
             }
             
             res0[which(ns == n), ] <- c(
@@ -53,9 +57,9 @@ seqBF <- function(cohensd = 0.5, prior = 0.5, nSims = 10, boundary = Inf, nmin =
                 boundary = boundary,
                 n		= n,
                 logBF	= logBF,
-                prior = prior / 2)
+                prior = prior)
             
-            if (abs(logBF) >= boundary) {break;}
+            if (abs(logBF) >= boundary | abs(logBF) <= 1 / boundary) {break;}
             
         }
         
@@ -65,7 +69,9 @@ seqBF <- function(cohensd = 0.5, prior = 0.5, nSims = 10, boundary = Inf, nmin =
     }
     
     class(res) <- c("resBF", "matrix")
-    res <- res[complete.cases(res),]
+    
+    # removing rows with only NAs
+    res <- res[!!rowSums(!is.na(res) ), ]
     
     return(res)
     
@@ -84,7 +90,7 @@ plot.resBF <- function(x, ...) {
         bty = "l", log = "y", 
         xlim = xlim, ylim = ylim, panel.first = grid(0, NULL, lty = 3) )
     
-    for(i in 1:length(unique(x$id) ) ){
+    for (i in 1:length(unique(x$id) ) ) {
         
         lines(x$logBF[x$id==i] ~ x$n[x$id==i], lwd = 0.6, col = "snow4" )
         
@@ -95,44 +101,64 @@ plot.resBF <- function(x, ...) {
 }
 
 #################################################################################
-# analyze results
+# analysing the results
 ######################################################
 
-# nSims is the number of experiments
-# prior represents the prior beliefs of the experimenter (between 0 (for H0) and 1 (for H1))
-# you can modify how the BF is affected by prior beliefs in the "if loop" lines 44-49
+# nSims is the number of simulated experiments
+# prior represents the prior beliefs of the experimenter (between 0 (for H0) and 1 (for H1) )
+# a prior of 0.5 represents neutral a priori expectancies while this penalisation can be removed
+# by specifying prior = NA
+# you can modify how the BF is affected by prior expectancies in the "if loop" lines 45-54
 
-analyse <- function(cohensd = 0.6, prior = 0.5, nSims = 100, boundary = 20, nmin = 10, nmax = 200, plot = TRUE){
+analyse <- function(cohensd = 0.5, prior = 0.5, nSims = 100, boundary = Inf, nmin = 10, nmax = 200, plot = TRUE){
     
     results <- seqBF(cohensd = cohensd, prior = prior, nSims = nSims, 
         boundary = boundary, nmin = nmin, nmax = nmax) %>% data.frame
     
     class(results) <-  c("resBF", "data.frame")
     
-    results$logBF[results$logBF>boundary] <- boundary
+    # ceiling the BFs over the boundary (for aesthetics purposes)
+    results$logBF[results$logBF >= boundary] <- boundary
+    results$logBF[results$logBF <= 1 / boundary] <- 1 / boundary
     
-    boundary_hit <- length(unique(results$id[results$logBF>=unique(results$boundary)]) )
-    all_traj <- length(unique(results$id) ) # number of simulations
-    percent <- (boundary_hit / all_traj) * 100 # percentage of sim hitting the boundary
-    asn <- mean(results$n[results$logBF>=boundary]) # average sample number (asn)
-    hit <- unique(results$n[results$logBF>=unique(results$boundary)])
+    # number of boundary hits
+    boundary_hit <-
+        results$id[results$logBF>=unique(results$boundary)|
+                results$logBF<=1/unique(results$boundary) ] %>%
+        unique %>% length
+    
+    # number of simulations
+    all_traj <- length(unique(results$id) )
+    # percentage of simulations hitting the boundary
+    percent <- (boundary_hit / all_traj) * 100
+    # average sample number (asn)
+    asn <- mean(results$n[results$logBF >= boundary|
+            results$logBF <= 1 / boundary])
+    # upper boundary
+    hit1 <- unique(results$n[results$logBF >= unique(results$boundary)])
+    # lower boundary
+    hit2 <- unique(results$n[results$logBF <= 1 / unique(results$boundary)])
     
     if(plot==TRUE){
         
         plot(results)
         abline(h = unique(results$boundary), lty = 2)
-        #text(max(results$n)/2, max(results$logBF)*1.5, paste0("boundary hit: ", percent, "%") )
-        #text(max(results$n)/2, max(results$logBF)*1.5, paste0("boundary hit: ", percent, "%,", " asn: ", round(asn, 0)  ) )
-        points(hit, rep(unique(results$boundary), length(hit)), pch = 20, cex = 1)
+        abline(h = 1 / unique(results$boundary), lty = 2)
+        #text(max(results$n)/2, max(results$logBF)*1.5,
+        #paste0("boundary hit: ",percent, "%,", " asn: ", round(asn, 0)  ) )
+        points(hit1, rep(unique(results$boundary), length(hit1) ), pch = 20, cex = 1)
+        points(hit2, rep(1 / unique(results$boundary), length(hit2) ), pch = 20, cex = 1)
         
     }
     
-    ana <- data.frame(cbind(unique(results$cohensd), unique(results$boundary), unique(results$prior), percent, asn))
+    ana <- data.frame(cbind(
+        unique(results$cohensd), unique(results$boundary),
+        unique(results$prior), percent, asn) )
+    
     colnames(ana) <- c("cohensd", "boundary", "prior", "percentage", "asn")
     
     return(ana)
     
 }
 
-analyse(cohensd = 0, prior = 0.01, nSims = 10, boundary = 20, nmin = 20, nmax = 100, plot = TRUE)
-analyse(cohensd = 0, prior = 0.99, nSims = 10, boundary = 20, nmin = 20, nmax = 100, plot = TRUE)
+analyse(cohensd = 0, prior = 0.5, nSims = 20, boundary = 6, nmin = 20, nmax = 100, plot = TRUE)
