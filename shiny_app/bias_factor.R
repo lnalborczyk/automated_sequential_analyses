a034dd8278e82bb582439e2668830d917b2420dd
lnalborczyk <ladislas.nalborczyk@gmail.com>
2021-03-03 11:15:04 +0100

    sync bias_factor.R accross subrepositories

--- shiny_app/bias_factor.R
+++ shiny_app/bias_factor.R
@@ -1,21 +1,20 @@
 #######################################################################
-# R code accompanying Beffara, Bret & Nalborczyk (2019)
-# OSF projet: https://osf.io/mwtvk/
+# R code accompanying Beffara, Bret & Nalborczyk (2021)
+# OSF project: https://osf.io/mwtvk/
 # ---------------------------------------------------------
 # Modelling SBF trajectories using a simple random-walk model
 # Adapted from https://github.com/psy-farrell/computational-modelling
 # Written by Ladislas Nalborczyk
 # E-mail: ladislas.nalborczyk@gmail.com
-# Last update: March 4, 2019
+# Last update: March 2, 2021
 #############################################################
 
-if (!require("hrbrthemes") ) install.packages("hrbrthemes"); library("hrbrthemes");
 if (!require("tidyverse") ) install.packages("tidyverse"); library("tidyverse");
 if (!require("patchwork") ) install.packages("patchwork"); library("patchwork");
 
 ##########################################################################
 # Parameters
-# --------------
+# --------------------------------------------------------------------
 # nsims: number of random walks (number of simulations/experiments)
 # nsamples: number of times evidence is being sampled (number of observations)
 # origin: starting value (should be 0 for a log(BF))
@@ -48,10 +47,10 @@ biasfactor <- function (
         
         # defines prior
         prior <- rnorm(1, prior, priorsd)
-        
+            
         # for each observation, compute BF
         for (j in 2:nsamples + 1) {
-            
+                
             # extracts last BF
             last <- evidence[i, j - 1]
             
@@ -62,10 +61,10 @@ biasfactor <- function (
             evidence[i, j] <- sum(last, rnorm(1, dr, bfsd) )
             
         }
-        
+                    
         # when does it hit the boundary ?
         p <- which(abs(evidence[i, ]) >= criterion)[1]
-        
+
         # lower or upper boundary ?
         responses[i] <- sign(evidence[i, p])
         
@@ -84,9 +83,80 @@ biasfactor <- function (
         mutate(
             boundary = rep(responses, each = nsamples + 1),
             latency = rep(latencies, each = nsamples + 1)
-        )
+            )
     
     # returning the results
     return(results)
     
 }
+
+#############################################################
+# simulating various scenarios
+#################################################
+
+# generating 100 SBF trajectories with no drift (no effect) and no bias
+plot1 <-
+    biasfactor(
+    nsims = 1e2, nsamples = 200, origin = 0, drift = 0, bfsd = 1,
+    criterion = 10, originsd = 0, driftsd = 0, prior = 0
+    ) %>%
+    ggplot(aes(x = obs, y = bf, group = sim) ) +
+    geom_hline(yintercept = 0, linetype = 2) +
+    geom_line(alpha = 0.3) +
+    theme_bw(base_size = 14) +
+    labs(
+        title = "Simulating the evolution of SBF trajectories",
+        subtitle = "Here for no effect (drift = 0) and no bias (prior = 0)",
+        x = "Number of observations", y = "log(BF)"
+        )
+
+# generating 100 SBF trajectories with no drift (no effect) and (medium) bias toward H1
+plot2 <-
+    biasfactor(
+    nsims = 1e2, nsamples = 200, origin = 0, drift = 0, bfsd = 1,
+    criterion = 10, originsd = 0, driftsd = 0, prior = 0.5
+    ) %>%
+    ggplot(aes(x = obs, y = bf, group = sim) ) +
+    geom_hline(yintercept = 0, linetype = 2) +
+    geom_line(alpha = 0.3) +
+    theme_bw(base_size = 14) +
+    labs(
+        title = "Simulating the evolution of SBF trajectories",
+        subtitle = "Here for no effect (drift = 0) and medium bias toward H1 (prior = 0.5)",
+        x = "Number of observations", y = "log(BF)"
+        )
+
+# generating 100 SBF trajectories with medium drift (medium effect) and (medium) bias toward H1
+plot3 <-
+    biasfactor(
+        nsims = 1e2, nsamples = 200, origin = 0, drift = 1, bfsd = 1,
+        criterion = 10, originsd = 0, driftsd = 0, prior = 0.5
+    ) %>%
+    ggplot(aes(x = obs, y = bf, group = sim) ) +
+    geom_hline(yintercept = 0, linetype = 2) +
+    geom_line(alpha = 0.3) +
+    theme_bw(base_size = 14) +
+    labs(
+        title = "Simulating the evolution of SBF trajectories",
+        subtitle = "Here for medium effect (drift = 1) and medium bias toward H1 (prior = 0.5)",
+        x = "Number of observations", y = "log(BF)"
+        )
+
+# generating 100 SBF trajectories with medium drift (medium effect) and (medium) bias toward H1
+plot4 <-
+    biasfactor(
+        nsims = 1e2, nsamples = 200, origin = 0, drift = 1, bfsd = 1,
+        criterion = 10, originsd = 0, driftsd = 0, prior = -0.5
+    ) %>%
+    ggplot(aes(x = obs, y = bf, group = sim) ) +
+    geom_hline(yintercept = 0, linetype = 2) +
+    geom_line(alpha = 0.3) +
+    theme_bw(base_size = 14) +
+    labs(
+        title = "Simulating the evolution of SBF trajectories",
+        subtitle = "Here for medium effect (drift = 1) and medium bias toward H0 (prior = -0.5)",
+        x = "Number of observations", y = "log(BF)"
+        )
+
+# combine plots using patchwork
+(plot1 + plot2) / (plot3 + plot4)
